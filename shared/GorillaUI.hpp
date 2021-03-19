ca7ae3570e947e314aec580e235cfb553ece8868
RedBrumbler <51795403+RedBrumbler@users.noreply.github.com>
2021-03-19 04:04:06 +0100

    forgot shared

---
+++ shared/GorillaUI.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "../shared/ViewLib/ViewManager.hpp"
+#include "../shared/ViewLib/View.hpp"
+
+namespace GorillaUI
+{
+    Components::ViewManager* CreateViewManager(Il2CppClass* klass);
+    Components::View* CreateView(Il2CppClass* klass);
+
+    template<class T = Components::View*>
+    T CreateView()
+    {
+        static_assert(std::is_convertible<T, Components::View*>());
+        return reinterpret_cast<T>(CreateView(classof(T)));
+    }
+
+    template<class T = Components::ViewManager*>
+    T CreateViewManager()
+    {
+        static_assert(std::is_convertible<T, Components::ViewManager*>());
+        return reinterpret_cast<T>(CreateViewManager(classof(T)));
+    }
+}   
